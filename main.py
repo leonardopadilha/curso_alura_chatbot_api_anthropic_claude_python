bb765e358a10158750f71a543795dcf0fb021e81
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-08-20 05:52:40 -0300

    Integração entre back e front

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template, request, Response 
+from bot import bot
+
+app = Flask(__name__)
+app.secret_key = 'alura'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    prompt = request.json['msg']
+    resposta = bot(prompt)
+    return resposta
+
+if __name__ == "__main__":
+    app.run(debug = True)
