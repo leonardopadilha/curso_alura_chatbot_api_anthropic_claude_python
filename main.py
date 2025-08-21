f6a8cd38c6dd7d1786fada01267fbb0dab0127e4
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-08-21 06:31:22 -0300

    Adicionando um histórico do assistente

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template, request, Response 
 from bot import bot
+import os
+from helpers import *
 
 app = Flask(__name__)
 app.secret_key = 'alura'
@@ -11,7 +13,17 @@ def home():
 @app.route("/chat", methods=["POST"])
 def chat():
     prompt = request.json['msg']
-    resposta = bot(prompt)
+    nome_do_arquivo = './historico/historico_SaborExpress.txt'
+    historico = ''
+    if os.path.exists(nome_do_arquivo):
+        historico = carrega(nome_do_arquivo)
+    resposta = bot(prompt, historico)
+    conteudo = f"""
+    Histórico: {historico}
+    Usuário: {prompt}
+    IA: {resposta}
+    """
+    salva(nome_do_arquivo, conteudo)
     return resposta
 
 if __name__ == "__main__":
